e41d566be787980722ae5f066b70236a7d57efac
Krishna Yangale <43802262+yangale444@users.noreply.github.com>
2022-01-26 18:23:40 +0530

    Update and rename ArrayMinMaxdiff.java to ArraySumMinMax.java

--- ArraySumMinMax.java
+++ ArraySumMinMax.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.Arrays;
 
-public class Main
+public class ArraySumMinMax
 {
 	public static void main(String[] args) {
 // 		System.out.println("Hello World");
